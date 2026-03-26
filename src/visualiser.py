7dd1cc5d618a3bf0295eb28fd3f2fb0547bdeffa
Frebagg <sever.kris@gmail.com>
2026-03-26 14:31:39 +0100

    dodan marker za ringe

--- src/visualiser.py
+++ src/visualiser.py
@@ -18,7 +18,11 @@ class visualizeMarkers(Node):
         self.faceCoordClient = self.create_subscription(FaceCoords,"/face_coords",self.manageFaceMarkers_callback,10)
         self.faceMarkerIds = []
 
+        self.ringCoordClient = self.create_subscription(FaceCoords,"/ring_coords",self.manageRingMarkers_callback,10)
+        self.ringMarkerIds = []
+
         self.markerPublisher = self.create_publisher(Marker,"/face_marker",10)
+        self.markerOffset = 100
 
         self.get_logger().info(f"Visualisation node initialized!")
 
@@ -65,6 +69,54 @@ class visualizeMarkers(Node):
             text.color.b = 1.0
             text.color.a = 1.0
             text.text = f"Face {id}"
+            text.pose.orientation.w = 1.0
+            text.action = Marker.ADD
+
+            self.markerPublisher.publish(text)
+            self.markerPublisher.publish(marker)
+
+        
+    def manageRingMarkers_callback(self,msg):
+        if len(msg.points) != len(msg.ids):
+            return
+
+        for ring, id in zip(msg.points, msg.ids):
+            if id in self.ringMarkerIds: #ce je ta obraz ze markiran ignoriraj
+                 continue
+            marker = Marker()
+            marker.header.frame_id = "map"
+            marker.header.stamp = self.get_clock().now().to_msg()
+            marker.ns = "rings"
+            marker.id = id + self.markerOffset
+            marker.type = Marker.CUBE
+            scale = 0.5
+            marker.scale.x = scale
+            marker.scale.y = scale
+            marker.scale.z = scale
+            marker.pose.position = ring
+            marker.color.r = 0.0
+            marker.color.g = 0.
+            marker.color.b = 1.0
+            marker.color.a = 1.0
+            marker.action = Marker.ADD
+            marker.pose.orientation.w = 1.0
+            self.ringMarkerIds.append(id)
+
+            text = Marker()
+            text.header.frame_id = "map"
+            text.header.stamp = self.get_clock().now().to_msg()
+            text.ns = "rings_label"
+            text.id = id + self.markerOffset
+            text.type = Marker.TEXT_VIEW_FACING
+            text.pose.position.x = ring.x
+            text.pose.position.y = ring.y
+            text.pose.position.z = ring.z + 0.5
+            text.scale.z = 0.5
+            text.color.r = 1.0
+            text.color.g = 1.0
+            text.color.b = 1.0
+            text.color.a = 1.0
+            text.text = f"Ring {id}"
             text.pose.orientation.w = 1.0
             text.action = Marker.ADD
 
